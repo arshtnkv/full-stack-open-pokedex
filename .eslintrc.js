5d5d0b8b43b0853c733dc77b78a3b13a546c09a0
Anton Reshetnikov <120567013+arshtnkv@users.noreply.github.com>
2024-12-14 22:34:43 +0300

    fix errors eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
